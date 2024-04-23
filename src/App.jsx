ba4286ec4b5b0f3cb7b9a97fb5b3836f2669ef9e
nitesh-20 <nsahu6156@gmail.com>
2024-04-23 22:00:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Pokedex from './Components/Pokedex/Pokedex'
+
+const App = () => {
+  return (
+    <div>
+      <Pokedex/>
+    </div>
+  )
+}
+
+export default App
